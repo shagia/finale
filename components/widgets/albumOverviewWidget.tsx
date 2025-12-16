c7e6b183c0feffec0e50cd929a1a3a694a2e129f
devinSG <devinjosephglover@gmail.com>
2025-12-16 15:46:48 -0800

    Resize the overview widget

--- components/widgets/albumOverviewWidget.tsx
+++ components/widgets/albumOverviewWidget.tsx
@@ -12,6 +12,12 @@ export const AlbumOverviewWidget = ({ itemOverview, audioMetadata }: AlbumOvervi
       style={{
         backgroundColor: "lightgray",
         width: 600,
+        height: 100,
+        maxHeight: 100,
+        overflow: "scroll",
+        padding: 10,
+        marginTop: 10,
+        marginBottom: 10,
         flexWrap: "wrap",
         flexShrink: 1,
         alignSelf: "center",
